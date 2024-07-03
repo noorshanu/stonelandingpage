7285f8faeb51cc92312f17168baf7b4503e29dce
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-07-04 02:36:32 +0530

    cbvcb

--- src/sections/JoinCommunity.jsx
+++ src/sections/JoinCommunity.jsx
@@ -1,18 +1,57 @@
 import { FaFacebookF, FaDiscord, FaInstagram } from "react-icons/fa";
 
-
-
-
-
-
 function JoinCommunity() {
   return (
-    <section className="pt-24 " id="contact">
+    <section className="pt-4 mt-4 bg-m2 " id="contact">
       <div className="  relative z-10 overflow-hidden container-wrapper">
-    
+        <img src="images/logo.png" alt="" className=" mx-auto" />
+        <div className=" grid grid-cols-2 sm:grid-cols-3 gap-4 justify-evenly">
+          <div>
+            <h1 className=" text-[#9DC2F9] font-bold text-xl ">LEARN MORE</h1>
 
-       
+            <div className=" flex flex-col gap-2">
+            <a href="/">About</a>
 
+<a href="/">About</a>
+
+<a href="/">About</a>
+
+<a href="/">About</a>
+<a href="/">About</a>
+<a href="/">About</a>
+            </div>
+          </div>
+          <div>
+            <h1 className=" text-[#9DC2F9] font-bold text-xl ">LEARN MORE</h1>
+
+          <div className=" flex flex-col gap-2">
+          <a href="/">About</a>
+
+<a href="/">About</a>
+
+<a href="/">About</a>
+
+<a href="/">About</a>
+<a href="/">About</a>
+<a href="/">About</a>
+          </div>
+          </div>
+          <div>
+            <h1 className=" text-[#9DC2F9] font-bold text-xl ">LEARN MORE</h1>
+            <div className=" flex flex-col gap-2">
+            <a href="/">About</a>
+
+            <a href="/">About</a>
+
+            <a href="/">About</a>
+
+            <a href="/">About</a>
+            <a href="/">About</a>
+            <a href="/">About</a>
+            </div>
+          </div>
+        </div>
+        <p className=" text-center text-sm py-8">©2023 Stone Wallet All rights reserved</p>
       </div>
     </section>
   );
--- src/sections/NewsLetter.jsx
+++ src/sections/NewsLetter.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function NewsLetter() {
   return (
-    <section>
+    <section className='py-6  sm:py-12'>
         <div  className=' container-wrapper '
         
         data-aos="fade-up"
             data-aos-offset="300"
             data-aos-duration="1500"
             data-aos-easing="ease-in-sine">
-            <h1 className=' text-center'>Receive our Newsletter
+            <h1 className=' text-center text-5xl font-bold pt-6'>Receive our Newsletter
             </h1>
-            <p className=' text-center'>
+            <p className=' text-center pb-10'>
             Sign up to receive updates and announcements
             </p>
          <div className=' flex justify-center'>
